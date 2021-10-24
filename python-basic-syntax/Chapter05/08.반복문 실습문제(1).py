06315921b8ccee1cdb907f8df319a709e84c44a0
manbalboy <manbalboy@hanmail.net>
2021-10-24 17:25:49 +0900

    반복문 실습문제1

---
+++ python-basic-syntax/Chapter05/08.반복문 실습문제(1).py
@@ -0,0 +1,18 @@
+# 구구단
+# dan = int(input("몇단을 출력할까요 >>> "))
+#
+# for number in range(1, 10):
+#     string = str(dan) + " * " + str(number) + " ="
+#     print(string, dan * number)
+
+while True:
+    menuNum = int(input("[메뉴를 입력하세요]\n1.게임시작 2.랭킹보기 3.게임종료 >>>"))
+    if menuNum == 1:
+        print("-> 게임을시작합니다.")
+    elif menuNum == 2:
+        print("-> 실시간 랭킹")
+    elif menuNum == 3:
+        print("게임을 종료합니다.")
+        break
+    else:
+        print("다시 입력하세요")
