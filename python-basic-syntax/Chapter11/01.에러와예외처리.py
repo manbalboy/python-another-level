4386f1cf8d20ed94859480b019bf38192ef99118
manbalboy <manbalboy@hanmail.net>
2021-10-29 00:41:31 +0900

    01 에러와 예외처리

---
+++ python-basic-syntax/Chapter11/01.에러와예외처리.py
@@ -0,0 +1,14 @@
+# 원화를 입력, 환율 입력  -> 달러값
+won = input("원화금액을 입력 하세요 >>>> ")
+dollar = input("환율을 입력 하세요 >>>> ")
+try:
+    # 예외가 발생할 수 있는 코드
+    print(int(won) / int(dollar))
+except ValueError as e:
+    print(e)
+else:
+    print("예외가 발생하지 않은 코드")
+finally:
+    print("항상 실행되는 코드 ")
+
+print("끝")
